586271c8a2de29ea74c1ed145767ee64052a3498
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-03-13 11:43:56 +0300

    prop-string tested in angular

--- property-string/ng-prop-string/src/app/app.component.ts
+++ property-string/ng-prop-string/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { getPropPath } from 'property-string-path';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-prop-string';
+export class AppComponent implements OnInit {
+  public title = 'ng-prop-string';
+  public form: FormGroup;
+
+  public controlPath = getPropPath<User>();
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.form = this.fb.group({
+      id: '',
+      name: '',
+      username: '',
+      email: '',
+      phone: '',
+      website: '',
+      company: this.fb.group({
+        name: '',
+        catchPhrase: '',
+        bs: '',
+      }),
+      address: this.fb.group({
+        street: '',
+        suite: '',
+        city: '',
+        zipcode: '',
+        geo: this.fb.group({
+          lat: '',
+          lng: '',
+        }),
+      }),
+    });
+
+    this.form.get(this.controlPath('address.geo.lat')).valueChanges.subscribe(console.log);
+  }
 }
