d57600bf21e6d79eaf43465202cf3e81a55d1f11
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-03-21 16:03:30 +0300

    added example

---
+++ eslint-plugin-react-css-module-hints/eslint-plugin-react-css-module-hints/src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import s from './App.module.css';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <header className={s["App-header"]}>
+        <img src={logo} className={s["App-logo"]} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={s["App-link"]}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
